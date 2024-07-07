d7d72d35623863212121f86e400c3a474a6aa115
circusbird789 <xgalletes@outlook.com>
2024-07-07 20:34:37 +0100

    refresh pt 2

--- module 4/SpeakGoodBye.js
+++ module 4/SpeakGoodBye.js
@@ -10,6 +10,7 @@ var byeSpeaker = (function () {
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 
+
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
 
